21361e8f2a0ecaa8bbd746257999bb6189c0089f
Cameron Kirkland <74569446+kircam89@users.noreply.github.com>
2021-10-16 22:42:42 -0500

    Add files via upload

---
+++ Allow O365 Users Access to On-Prem Shared Mailboxes.ps1
@@ -0,0 +1,8 @@
+#Run on prem:
+Add-ADPermission -Identity "JohnDoe@domain.com" -User "JohnDoe" -AccessRights ExtendedRight -ExtendedRights "Send As"
+
+#Run through exchange online powershell
+Add-RecipientPermission -Identity "All Email Users" -Trustee "JohnDoe" -AccessRights SendAs
+
+#Note: The second command will only work if you are already connected to exchange online in powershell.  If you are not, run this command first:
+Connect-ExchangeOnline -UserPrincipalName JaneDow@domain.onmicrosoft.com -ShowProgress $true
