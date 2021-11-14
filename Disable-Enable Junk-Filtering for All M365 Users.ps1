89bac9374cc0814e53d2115b811bc4ef7b3167ac
Cameron Kirkland <74569446+kircam89@users.noreply.github.com>
2021-11-14 00:18:49 -0600

    Add files via upload

---
+++ Disable-Enable Junk-Filtering for All M365 Users.ps1
@@ -0,0 +1,10 @@
+
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session
+
+Get-Mailbox
+
+Get-Mailbox | Set-MailboxJunkEmailConfiguration –Enabled $False
