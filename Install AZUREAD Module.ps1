8500057facb263eb1da748e68e696eb9209f64f8
Cameron Kirkland <74569446+kircam89@users.noreply.github.com>
2021-11-14 00:50:16 -0600

    Add files via upload

---
+++ Install AZUREAD Module.ps1
@@ -0,0 +1,5 @@
+#If your computer has all the prerequisites for the installation, to install the General Availability version of the module on your computer
+Install-Module AzureAD
+
+#To install the public preview release
+Install-Module AzureADPreview
