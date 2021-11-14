b61880a3d0cd0e137993093bd73e22a5a6830161
Cameron Kirkland <74569446+kircam89@users.noreply.github.com>
2021-11-14 00:27:20 -0600

    Add files via upload

---
+++ Enable Mail Forward via Exchange Management Shell.ps1
@@ -0,0 +1,4 @@
+-Set-Mailbox -Identity "Douglas Kohn" -DeliverToMailboxAndForward $true -ForwardingSMTPAddress "douglaskohn.parents@fineartschool.net"
+
+
+-Set-Mailbox -Identity "Ken Sanchez" -ForwardingAddress "pilarp@contoso.com"
