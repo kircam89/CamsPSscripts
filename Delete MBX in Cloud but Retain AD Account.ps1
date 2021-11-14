3d709f89672bf1e399dec7bb8f1a47b6187662d8
Cameron Kirkland <74569446+kircam89@users.noreply.github.com>
2021-11-14 00:04:56 -0600

    Add files via upload

---
+++ Delete MBX in Cloud but Retain AD Account.ps1
@@ -0,0 +1,6 @@
+#This cmdlet is available only in on-premises Exchange.
+
+#Use the Disable-RemoteMailbox cmdlet to remove user mailboxes from the cloud-based service 
+#but keep the associated user objects in the on-premises Active Directory.
+
+Disable-RemoteMailbox -Identity John.Doe@Incognitocorp.com
